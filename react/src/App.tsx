75da68071a110da77a03b05e92913b2f3d2eb7aa
zachar-AI <zachskihill@gmail.com>
2026-01-16 23:17:09 -0500

    just in case

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,8 +1,3 @@
-import { useEffect, useRef, useState} from "react";
-import { Knob } from "./components/Knob";
-import { ControlStrip } from "./components/ControlStrip"
-import { LayerStrip } from "./components/LayerStrip"
-import type { LayerStripProps } from "./types/types";
 import { Daw } from "./components/Daw"
 import "./App.css";
 
