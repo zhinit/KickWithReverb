22d29430cef7fcb8e1c4e640f0c25e8bbd39b418
zachar-AI <zachskihill@gmail.com>
2026-01-22 12:28:23 -0500

    for some reason login buttons are still deployed even though not on github. tried removeing lines o code rather than commenting. if this doesnt work, Ill just move on

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,11 +1,9 @@
 import { Daw } from "./components/Daw"
 import "./App.css";
-import { LoginRegister } from "./components/LoginRegister";
 
 function App() {
   return (
     <>
-      {/* <LoginRegister /> */}
       <Daw />
     </>
   );
