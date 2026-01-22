366afcee4f967add799e205103159f5ad6d07847
zachar-AI <zachskihill@gmail.com>
2026-01-22 12:14:56 -0500

    added log in and sign up buttons.

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,9 +1,11 @@
 import { Daw } from "./components/Daw"
 import "./App.css";
+import { LoginRegister } from "./components/LoginRegister";
 
 function App() {
   return (
     <>
+      <LoginRegister />
       <Daw />
     </>
   );
---
+++ react/src/components/LoginRegister.tsx
@@ -0,0 +1,6 @@
+export const LoginRegister = () => (
+  <div className="login-register">
+    <button className="login-button">Log In</button>
+    <button className="login-button">Sign Up</button>
+  </div>
+)
