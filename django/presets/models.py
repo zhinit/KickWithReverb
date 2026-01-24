83eaf4c02246b5af67e3fbb8182ab04d415b2a13
zachar-AI <zachskihill@gmail.com>
2026-01-23 21:58:55 -0500

    made preset django models and migrated

--- django/presets/models.py
+++ django/presets/models.py
@@ -1,3 +1,72 @@
+from enum import unique
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Preset(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    preset_name = models.CharField(max_length=32)
+
+    bpm = models.PositiveSmallIntegerField()
+
+    kick_sample = models.CharField(max_length=32)
+    kick_len = models.FloatField()
+    kick_dist_amt = models.FloatField()
+    kick_ott_amt = models.FloatField()
+
+    noise_sample = models.CharField(max_length=32)
+    noise_low_pass_freq = models.FloatField()
+    noise_high_pass_freq = models.FloatField()
+    noise_dist_amt = models.FloatField()
+
+    reverb_sample = models.CharField(max_length=32)
+    reverb_low_pass_freq = models.FloatField()
+    reverb_high_pass_freq = models.FloatField()
+    reverb_phaser_amt = models.FloatField()
+
+    master_ott_amt = models.FloatField()
+    master_dist_amt = models.FloatField()
+    master_limiter_amt = models.FloatField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username}, {self.preset_name}"
+
+    class Meta:
+        unique_together = ["user", "preset_name"]
+        ordering = ["preset_name"]
+
+
+class SharedPreset(models.Model):
+    preset_name = models.CharField(max_length=32, unique=True)
+
+    bpm = models.PositiveSmallIntegerField()
+
+    kick_sample = models.CharField(max_length=32)
+    kick_len = models.FloatField()
+    kick_dist_amt = models.FloatField()
+    kick_ott_amt = models.FloatField()
+
+    noise_sample = models.CharField(max_length=32)
+    noise_low_pass_freq = models.FloatField()
+    noise_high_pass_freq = models.FloatField()
+    noise_dist_amt = models.FloatField()
+
+    reverb_sample = models.CharField(max_length=32)
+    reverb_low_pass_freq = models.FloatField()
+    reverb_high_pass_freq = models.FloatField()
+    reverb_phaser_amt = models.FloatField()
+
+    master_ott_amt = models.FloatField()
+    master_dist_amt = models.FloatField()
+    master_limiter_amt = models.FloatField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.preset_name
+
+    class Meta:
+        ordering = ["preset_name"]
