8d8ba6cd2131c7a4f8593b52bf44f93888ed2f35
zachar-AI <zachskihill@gmail.com>
2026-01-26 23:08:54 -0500

    more thourough deleting of shared preset model including views and imports

--- django/presets/admin.py
+++ django/presets/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import Preset, SharedPreset
+from .models import Preset
 
 admin.site.register(Preset)
-admin.site.register(SharedPreset)
--- django/presets/serializers.py
+++ django/presets/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Preset, SharedPreset
+from .models import Preset
 
 
 class PresetSerializer(serializers.ModelSerializer):
