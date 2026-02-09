319e18a6568367bed64f77711ce098685451c1e9
zachar-AI <zachskihill@gmail.com>
2026-02-08 22:38:43 -0500

    added models.py to kickgen django app which defines schema for generated kick object. it points to supabase bucket

--- django/kickgen/models.py
+++ django/kickgen/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class GeneratedKick(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+    audio_url = models.URLField()
+    storage_path = models.CharField(max_length=256)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}, {self.name}"
+
+    class Meta:
+        ordering = ["name"]
