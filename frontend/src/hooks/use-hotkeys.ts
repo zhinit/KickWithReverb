dbcdc740821a5e13f1bc0e5ea6dd3f7604b7b19a
zachar-AI <zachskihill@gmail.com>
2026-03-04 00:33:51 -0500

    added hotkey for cue button

--- frontend/src/hooks/use-hotkeys.ts
+++ frontend/src/hooks/use-hotkeys.ts
@@ -1,15 +1,34 @@
 import { useEffect } from "react";
 
-export const useHotkeys = (transport: { handlePlayClick: Function }) => {
+export const useHotkeys = (transport: {
+  handlePlayClick: Function;
+  handleCueMouseDown: Function;
+  handleCueMouseUp: Function;
+}) => {
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
-      if (e.key === " ") {
-        transport.handlePlayClick();
+      switch (e.key) {
+        case " ":
+          transport.handlePlayClick();
+          break;
+        case "Enter":
+          transport.handleCueMouseDown();
+          break;
+      }
+    };
+
+    const handleKeyUp = (e: KeyboardEvent) => {
+      if (e.key === "Enter") {
+        transport.handleCueMouseUp();
       }
     };
 
     window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
 
-    return () => window.removeEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("keyup", handleKeyUp);
+    };
   }, [transport.handlePlayClick]);
 };
