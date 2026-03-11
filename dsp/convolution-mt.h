b5e3c5f3b7e36807a96bfff06bc09c725804fb5f
zachar-AI <zachskihill@gmail.com>
2026-03-11 17:21:43 -0400

    first version of mt conv header is done

--- dsp/convolution-mt.h
+++ dsp/convolution-mt.h
@@ -1,34 +1,46 @@
 #pragma once
 #include <cstddef>
+#include <juce_dsp/juce_dsp.h>
+#include <vector>
 
 class EarlyConvolutionEngine
 {
 public:
-  void loadIR(const float* irData,
-              const size_t irLength,
-              const size_t numChannels);
-  void process(const float* leftIn,
-               const float* rightIn,
-               float* leftOut,
-               float* rightOut,
-               const size_t numSamples);
+  void loadIR(const float* irData, const size_t irLength);
+  void process(const float* input, float* output, const size_t numSamples);
   void reset();
 
 private:
+  size_t numIrSegments_;
+  size_t currSegment_;
+  std::vector<std::vector<float>> irSegmentsFFT_;
+  std::vector<std::vector<float>> inputHistoryFFT_;
+  std::vector<float> overlapBuffer_;
+
+  static constexpr int fftOrder_ = 9; // 2^9 = 512
+  static constexpr size_t fftSize_ = 512;
+  static constexpr size_t blockSize_ = 128;
+
+  juce::dsp::FFT fft_{ fftOrder_ };
 };
 
 class LateConvolutionEngine
 {
 public:
-  void loadIR(const float* irData,
-              const size_t irLength,
-              const size_t numChannels);
-  void process(const float* leftIn,
-               const float* rightIn,
-               float* leftOut,
-               float* rightOut,
-               const size_t numSamples);
+  void loadIR(const float* irData, const size_t irLength);
+  void process(const float* input, float* output, const size_t numSamples);
   void reset();
 
 private:
+  size_t numIrSegments_;
+  size_t currSegment_;
+  std::vector<std::vector<float>> irSegmentsFFT_;
+  std::vector<std::vector<float>> inputHistoryFFT_;
+  std::vector<float> overlapBuffer_;
+
+  static constexpr int fftOrder_ = 9; // 2^9 = 512
+  static constexpr size_t fftSize_ = 512;
+  static constexpr size_t blockSize_ = 128;
+
+  juce::dsp::FFT fft_{ fftOrder_ };
 };
