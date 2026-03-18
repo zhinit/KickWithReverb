431d6375fedc61fcad9d068f869e0dbb2f41e270
zachar-AI <zachskihill@gmail.com>
2026-03-17 22:13:16 -0400

    late convolution worker should be all setup now

---
+++ frontend/public/late-convolution-worker.js
@@ -0,0 +1,57 @@
+importScripts("./audio-engine.js");
+
+let wasmReady = false;
+let Module;
+let lateReverb;
+
+createAudioEngine().then((mod) => {
+  Module = mod;
+  wasmReady = true;
+  lateReverb = new Module.LateStereoConvolutionReverb();
+});
+
+self.onmessage = (e) => {
+  if (!wasmReady) return;
+
+  const { type, data } = e.data;
+
+  switch (type) {
+    case "loadIR": {
+      const ptr = Module._malloc(data.samples.length * 4);
+      Module.HEAPF32.set(data.samples, ptr / 4);
+
+      lateReverb.loadIR(
+        ptr,
+        data.samples.length / data.numChannels,
+        data.numChannels,
+      );
+
+      Module._free(ptr);
+
+      break;
+    }
+    case "process": {
+      const ptrLeft = Module._malloc(data.left.length * 4);
+      Module.HEAPF32.set(data.left, ptrLeft / 4);
+      const ptrRight = Module._malloc(data.right.length * 4);
+      Module.HEAPF32.set(data.right, ptrRight / 4);
+
+      lateReverb.process(ptrLeft, ptrRight, 128);
+      const wetLeft = Module.HEAPF32.slice(ptrLeft / 4, ptrLeft / 4 + 128);
+      const wetRight = Module.HEAPF32.slice(ptrRight / 4, ptrRight / 4 + 128);
+      self.postMessage({ type: "process", left: wetLeft, right: wetRight }, [
+        wetLeft.buffer,
+        wetRight.buffer,
+      ]);
+
+      Module._free(ptrLeft);
+      Module._free(ptrRight);
+
+      break;
+    }
+    case "reset": {
+      lateReverb.reset();
+      break;
+    }
+  }
+};
